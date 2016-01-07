4bb7e15050237ec0f8e40e078476edff2052b3e8
L3E8S0S6E5N1OA5N6D3T8W5O <Disslove77777@gmail.com>
2016-01-07 20:37:09 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -220,7 +220,7 @@ function create_config( )
     "invite",
     "all"
     },
-    sudo_users = {168589520,169530637},--Sudo users
+    sudo_users = { 168589520 , 169530637 },--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
